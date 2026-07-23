1d87202551538e0724389df23430a459452ac962
TomoyaKanno <14018139+TomoyaKanno@users.noreply.github.com>
2026-07-23 10:03:55 -0400

    Use Observable metrics module suffix

--- observable/src/components/RecentStrain.tsx
+++ observable/src/components/RecentStrain.tsx
@@ -1,5 +1,5 @@
 import type {CSSProperties} from "npm:react";
-import {BULLPEN_TOTAL_WINDOWS, trailingPitchTotal} from "./metrics.ts";
+import {BULLPEN_TOTAL_WINDOWS, trailingPitchTotal} from "./metrics.js";
 
 export interface RecentPitcher {
   pitcher_id: number;
